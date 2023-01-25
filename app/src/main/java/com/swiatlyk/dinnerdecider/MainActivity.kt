8146bff5efc10f233e9128cf727e8f113cbe2a2f
Krzysztof <swiatlyk@gmail.com>
2023-01-25 20:11:28 +0100

    Added creating view by findViewByID

--- app/src/main/java/com/swiatlyk/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/swiatlyk/dinnerdecider/MainActivity.kt
@@ -1,33 +1,52 @@
 package com.swiatlyk.dinnerdecider
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.swiatlyk.dinnerdecider.databinding.ActivityMainBinding
+//import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    val foodList = arrayListOf("Chines", "Pizza", "Kebab","Ramen", "KFC")
+//    private lateinit var binding: ActivityMainBinding
+    private val foodList = arrayListOf("Chines", "Pizza", "Kebab","Ramen", "KFC")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        var selectedFoodTxt = findViewById<TextView>(R.id.selectedFoodTxt)
+        var decideButton = findViewById<Button>(R.id.decideButton)
+        var addFoodTxt = findViewById<EditText>(R.id.addFoodTxt)
+        var addFoodBtn = findViewById<Button>(R.id.addFoodBtn)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        val view = binding.root
+//        setContentView(view)
 
-//        binding.selectedFoodTxt.text = "Burger King"
-        binding.decideButton.setOnClickListener {
+        decideButton.setOnClickListener {
             val random = Random()
             val randomFood = random.nextInt(foodList.count())
-            binding.selectedFoodTxt.text = foodList[randomFood]
+            selectedFoodTxt.text = foodList[randomFood]
         }
 
-        binding.addFoodBtn.setOnClickListener {
-            val newFood = binding.addFoodTxt.text.toString()
+//        binding.selectedFoodTxt.text = "Burger King"
+//        binding.decideButton.setOnClickListener {
+//            val random = Random()
+//            val randomFood = random.nextInt(foodList.count())
+//            binding.selectedFoodTxt.text = foodList[randomFood]
+//        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
             foodList.add(newFood)
-            binding.addFoodTxt.text.clear()
+            addFoodTxt.text.clear()
         }
+
+//        binding.addFoodBtn.setOnClickListener {
+//            val newFood = binding.addFoodTxt.text.toString()
+//            foodList.add(newFood)
+//            binding.addFoodTxt.text.clear()
+//        }
     }
 }
